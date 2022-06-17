3c03513e4742c59254dd02730efcaf76544d6437
tom <liuminkai2021@163.com>
2022-06-17 11:13:14 +0800

    copy Eureka-01快速入门-5.* to Eureka-03高可用

---
+++ code/springcloud-01-eureka/03高可用/spring-cloud-parent/eureka-provider/src/main/java/cn/liuminkai/provider/controller/GoodsController.java
@@ -0,0 +1,30 @@
+package cn.liuminkai.provider.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import cn.liuminkai.provider.pojo.Goods;
+import cn.liuminkai.provider.service.GoodsService;
+
+@SuppressWarnings("ALL")
+@RestController
+@RequestMapping
+public class GoodsController {
+
+    @Autowired
+    private GoodsService goodsService;
+
+    @GetMapping
+    public String index() {
+        return "eureka-provider goods";
+    }
+
+    @GetMapping("/goods/{id}")
+    public Goods findOne(@PathVariable("id") int id) {
+        Goods one = goodsService.findOne(id);
+        return one;
+    }
+}
+
